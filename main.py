7a808e0da7a26379b5108e073e4f663bfb5fd2ed
Luis Fernando Planella Gonzalez <luis@cyclos.org>
2025-07-01 16:18:05 -0300

    Another fix to CORS

--- main.py
+++ main.py
@@ -99,6 +99,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         try:
             self.send_response(200)
             self.send_header('Allow', 'GET, POST, OPTIONS')
+            self.send_header('Access-Control-Allow-Origin', 'https://app.element.io')
             self.send_header('Connection', 'close')
             self.end_headers()
         except (BrokenPipeError, ConnectionResetError):
